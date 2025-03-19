bc2c948c33742e4cc92d2e409628154f5b85a497
Sumit DevOps <SumitDevOps@gmail.com>
2025-03-19 15:44:38 +0530

    changed the subnet cidr

--- variables.tf
+++ variables.tf
@@ -8,6 +8,6 @@ variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
   default     = [
-    "10.0.1.0/24"  # You can pick a single subnet for simplicity
+    "10.0.5.0/24"  # You can pick a single subnet for simplicity
   ]
 }
